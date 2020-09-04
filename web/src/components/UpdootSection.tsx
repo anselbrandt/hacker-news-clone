161874ed776fd5bf06e9ee987be822a68fc21675
anselbrandt <ansel@anselbrandt.com>
2020-09-04 04:14:21 -0400

    Add auth check on upvote/downvote

--- web/src/components/UpdootSection.tsx
+++ web/src/components/UpdootSection.tsx
@@ -4,9 +4,11 @@ import {
   PostSnippetFragment,
   useVoteMutation,
   VoteMutation,
+  useMeQuery,
 } from "../generated/graphql";
 import gql from "graphql-tag";
 import { ApolloCache } from "@apollo/client";
+import { useRouter } from "next/router";
 
 interface UpdootSectionProps {
   post: PostSnippetFragment;
@@ -51,6 +53,8 @@ const updateAfterVote = (
 };
 
 export const UpdootSection: React.FC<UpdootSectionProps> = ({ post }) => {
+  const router = useRouter();
+  const { data: meData } = useMeQuery();
   const [loadingState, setLoadingState] = useState<
     "updoot-loading" | "downdoot-loading" | "not-loading"
   >("not-loading");
@@ -59,18 +63,21 @@ export const UpdootSection: React.FC<UpdootSectionProps> = ({ post }) => {
     <Flex direction="column" justifyContent="center" alignItems="center" mr={4}>
       <IconButton
         onClick={async () => {
-          if (post.voteStatus === 1) {
+          if (!meData?.me?.id) {
+            router.push("/login");
+          } else if (post.voteStatus === 1) {
             return;
+          } else {
+            setLoadingState("updoot-loading");
+            await vote({
+              variables: {
+                postId: post.id,
+                value: 1,
+              },
+              update: (cache) => updateAfterVote(1, post.id, cache),
+            });
+            setLoadingState("not-loading");
           }
-          setLoadingState("updoot-loading");
-          await vote({
-            variables: {
-              postId: post.id,
-              value: 1,
-            },
-            update: (cache) => updateAfterVote(1, post.id, cache),
-          });
-          setLoadingState("not-loading");
         }}
         variant={post.voteStatus ? "outline" : "ghost"}
         variantColor={post.voteStatus === 1 ? "green" : undefined}
@@ -81,18 +88,21 @@ export const UpdootSection: React.FC<UpdootSectionProps> = ({ post }) => {
       {post.points !== 0 ? post.points : null}
       <IconButton
         onClick={async () => {
-          if (post.voteStatus === -1) {
+          if (!meData?.me?.id) {
+            router.push("/login");
+          } else if (post.voteStatus === -1) {
             return;
+          } else {
+            setLoadingState("downdoot-loading");
+            await vote({
+              variables: {
+                postId: post.id,
+                value: -1,
+              },
+              update: (cache) => updateAfterVote(-1, post.id, cache),
+            });
+            setLoadingState("not-loading");
           }
-          setLoadingState("downdoot-loading");
-          await vote({
-            variables: {
-              postId: post.id,
-              value: -1,
-            },
-            update: (cache) => updateAfterVote(-1, post.id, cache),
-          });
-          setLoadingState("not-loading");
         }}
         variant={post.voteStatus ? "outline" : "ghost"}
         variantColor={post.voteStatus === -1 ? "red" : undefined}
