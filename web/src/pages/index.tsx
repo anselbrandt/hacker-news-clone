2c03507886f592eb55c1387573c6de85088ef829
anselbrandt <ansel@anselbrandt.com>
2020-09-05 03:20:11 -0400

    Remove unused import

--- web/src/pages/index.tsx
+++ web/src/pages/index.tsx
@@ -13,7 +13,6 @@ import {
 import { Hero } from "../components/Hero";
 import { Container } from "../components/Container";
 import { Main } from "../components/Main";
-import { DarkModeSwitch } from "../components/DarkModeSwitch";
 import { CTA } from "../components/CTA";
 import { Footer } from "../components/Footer";
 import { Navbar } from "../components/Navbar";
